ab25eb6615adac96e8a642d6089619c8b27ef206
Peter Phan <peterphan_1996@live.com>
2020-07-12 23:06:24 +0700

    Remove center title in mobile screens

--- components/post-title.js
+++ components/post-title.js
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
   return (
-    <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold tracking-tighter leading-tight md:leading-none mb-6 text-center md:text-left mt-10">
+    <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold tracking-tighter leading-tight md:leading-none mb-6 md:text-left mt-10">
       {children}
     </h1>
   );
